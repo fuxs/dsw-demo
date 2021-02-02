9806daccb64146a89fb4ed75d63e05b228fe2340
fuxs <michael@bungenstock.de>
2021-02-02 17:49:54 +0000

    Demo commit

---
+++ .ipynb_checkpoints/mylib-checkpoint.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# This is a demo loader
+def my_data_loader():
+    dataset_csv_url = "https://raw.githubusercontent.com/adobe/acp-data-services-dsw-reference/master/datasets/retail/retail.csv"
+
+    # Load data into a Pandas DataFrame
+    df = pd.read_csv(dataset_csv_url)
+    return df
