f3a3c9a49febedac4381f069873fcda8ba49819e
fuxs <michael@bungenstock.de>
2021-02-02 18:51:27 +0100

    New Commit

--- mylib.py
+++ mylib.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 # This is a demo loader
+# Change back
 def my_data_loader():
     dataset_csv_url = "https://raw.githubusercontent.com/adobe/acp-data-services-dsw-reference/master/datasets/retail/retail.csv"
 
